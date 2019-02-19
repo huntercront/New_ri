d629d08eceb4aa04a07b9cf12673a704b5ead493
huntercront <38252870+huntercront@users.noreply.github.com>
2019-02-19 15:20:24 +0300

    add adaptive

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ var btnm = document.getElementById("m_memu_text");
 		btnm.classList.toggle("manu-active");
 					btn.classList.toggle("buttion-active");
 			}
+
+
+			var wow = new WOW(
+				{
+					boxClass:     'facts-container-counter-il-anim',      // класс, скрывающий элемент до момента отображения на экране (по умолчанию, wow)
+					animateClass: 'anim-70',
+					animateClass: 'anim-60',
+					animateClass: 'anim-50',
+					animateClass: 'anim-40',
+					animateClass: 'anim-30',
+					animateClass: 'anim-20',
+					animateClass: 'anim-10',
+					animateClass: 'anim-5', // класс для анимации элемента (по умолчанию, animated)
+					mobile:       true,       // включение/отключение WOW.js на мобильных устройствах (по умолчанию, включено)
+					live:         true,       // поддержка асинхронно загруженных элементов (по умолчанию, включена)
+					callback:     function(box) {
+						// функция срабатывает каждый раз при старте анимации
+						// аргумент box — элемент, для которого была запущена анимация
+					},
+					scrollContainer: null // селектор прокручивающегося контейнера (опционально, по умолчанию, window)
+				}
+			);
+			wow.init();
