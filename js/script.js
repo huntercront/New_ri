4c6f337670bcab6ad7b4c7ba72e4cdb6a4d0bb2f
huntercront <38252870+huntercront@users.noreply.github.com>
2019-02-23 17:27:52 +0300

    add map

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ var btnm = document.getElementById("m_memu_text");
 		btnm.classList.toggle("manu-active");
 					btn.classList.toggle("buttion-active");
 			}
-
-
 			var wow = new WOW(
 				{
 					boxClass:     'facts-container-counter-il-anim',    // класс, скрывающий элемент до момента отображения на экране (по умолчанию, wow)
@@ -26,7 +24,7 @@ var btnm = document.getElementById("m_memu_text");
 					scrollContainer: null // селектор прокручивающегося контейнера (опционально, по умолчанию, window)
 				}
 			);
-
+			wow.init();
 
 
 var russia_svg = document.getElementById('russia');
@@ -116,14 +114,13 @@ function myFunction(x) {
 	if (x.matches) {
 		var testimonials_section = document.getElementById("testimonials");
 	testimonials_section.classList.add("siema");
-	wow.init();
 	const mySiema = new Siema({
 		loop: true,
 		selector: '.siema',
 		duration: 700,
 		easing: 'ease-in-out',
 		perPage: 1,
-		startIndex: 0,
+		startIndex: 3,
 		draggable: true,
 		multipleDrag: false,
 		threshold: 20,
@@ -134,13 +131,109 @@ function myFunction(x) {
 	document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
 	document.querySelector('.next').addEventListener('click', () => mySiema.next()); 
 }
-	else
-	{
-		var testimonials_section = document.getElementsByClassName("comment-container");
-		testimonials_section.classList.remove("siema");
-Array.prototype.forEach.call(testimonials_section, function(element){
-    element.removeAttribute('style');
-});
-
 }
+
+
+var y = window.matchMedia("(max-width: 768px)")
+myFunction2(y)
+y.addListener(myFunction2)
+function myFunction2(y) {
+	if (y.matches) {
+		var news_section = document.getElementById("news-block");
+	news_section.classList.add("siema-news");
+	var newSiema = new Siema({
+		loop: true,
+		selector: '.siema-news',
+		duration: 700,
+		easing: 'ease-in-out',
+		perPage: 2,
+		startIndex: 0,
+		draggable: true,
+		multipleDrag: false,
+		threshold: 20,
+		rtl: false,
+		onInit: () => {},
+		onChange: () => {},
+	});
+	document.querySelector('.prev-news').addEventListener('click', () => newSiema.prev());
+	document.querySelector('.next-news').addEventListener('click', () => newSiema.next()); 
+}
+}
+
+
+ymaps.ready(init);
+
+var placemarks = [
+    {
+        latitude: 59.97,
+        longitude: 30.31,
+        hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 19',
+            '</div>'
+        ]
+    },
+    {
+        latitude: 59.94,
+        longitude: 30.25,
+        hintContent: '<div class="map__hint">Малый проспект В О, д 64</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: Малый проспект В О, д 64',
+            '</div>'
+        ]
+    },
+    {
+        latitude: 59.93,
+        longitude: 30.34,
+        hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+            'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
+            '</div>'
+        ]
+    }
+],
+    geoObjects= [];
+
+function init() {
+    var map = new ymaps.Map('map', {
+        center: [59.94, 30.32],
+        zoom: 12,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+
+    for (var i = 0; i < placemarks.length; i++) {
+            geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
+            {
+                hintContent: placemarks[i].hintContent,
+                balloonContent: placemarks[i].balloonContent.join('')
+            },
+            {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/sprite.png',
+                iconImageSize: [46, 57],
+                iconImageOffset: [-23, -57],
+                iconImageClipRect: [[415, 0], [461, 57]]
+            });
+    }
+
+    var clusterer = new ymaps.Clusterer({
+        clusterIcons: [
+            {
+                href: 'img/burger.png',
+                size: [100, 100],
+                offset: [-50, -50]
+            }
+        ],
+        clusterIconContentLayout: null
+    });
+
+    map.geoObjects.add(clusterer);
+    clusterer.add(geoObjects);
 }
